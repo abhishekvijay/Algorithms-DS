0bd2755b842a1dd73682ef36c688a42a8b3f4963
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-04-03 07:34:50 +0530

    BSort

---
+++ Sorting/BSort.cpp
@@ -0,0 +1,75 @@
+
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void partition(int arr[], int lb, int ub, int *val)
+{
+	int a = arr[lb];
+	int llb = lb;
+	int uub = ub;
+
+	while(llb < uub)
+	{
+		while(arr[llb] <= a && llb < uub)
+		{
+			llb++;
+
+			if (arr[llb] < arr[llb-1])
+				swap(&arr[llb], &arr[llb-1]);
+			
+		}
+		while(arr[uub] > a)
+		{
+			uub--;
+
+			if (arr[uub] > arr[uub+1])
+				swap(&arr[uub], &arr[uub+1]);			
+		}
+
+		if (llb < uub) {
+			swap(&arr[llb], &arr[uub]);
+
+			if (arr[llb] < arr[llb-1])
+				swap(&arr[llb], &arr[llb-1]);
+
+			if (arr[uub] > arr[uub+1])
+				swap(&arr[uub], &arr[uub+1]);
+		}
+	}
+
+	arr[lb] = arr[uub];
+	arr[uub] = a;
+	*val = uub;
+}
+
+void BSort(int arr[], int st, int en)
+{
+	if (en <= st)
+		return;
+
+	int j = 0;
+
+	partition(arr, st, en, &j);
+
+	BSort(arr, st, j-1);
+
+	BSort(arr, j+1, en);
+}
+
+int main()
+{
+	int arr[10] = {23,45,12,89,34,75,99,62,92,16};
+
+	BSort(arr, 0, 9);
+
+	for(register int i = 0; i<10; ++i)
+		printf("%d ", arr[i]);
+
+	return 0;
+}
