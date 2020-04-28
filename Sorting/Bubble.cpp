bc145b0153c1a25bc29c6cc8bdbb5b875fa70315
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-04-28 14:27:59 +0530

    optimized bubble sort

--- Sorting/Bubble.cpp
+++ Sorting/Bubble.cpp
@@ -1,4 +1,4 @@
-
+#if 1
 #include <stdio.h>
 
 void swap(int *a, int *b)
@@ -6,6 +6,30 @@ void swap(int *a, int *b)
 	int temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+void optimized_bubble_sort(int arr[], int st, int en)
+{
+	if (en <= st)
+		return;
+
+	int *ptr = arr;
+	int pass, i;
+	int IsFound = 1;
+
+	for(pass = 0; pass < en-1 && IsFound; ++pass)
+	{
+		IsFound = 0;
+
+		for(i=0; i<en-pass-1; ++i) //applied opitmization
+		{
+			if (ptr[i] > ptr[i+1])
+			{
+				IsFound = 1;
+				swap(&ptr[i], &ptr[i+1]);
+			}
+		}
+	}
 }
 
 void sort_bubble(int arr[], int st, int en)
@@ -33,12 +57,14 @@ void sort_bubble(int arr[], int st, int en)
 
 int main()
 {
-	int arr[10] = {67,45,32,91,61,72,89,45,12,34};
+	int arr[10] = {67,100,32,91,61,72,89,45,12,34};
 
-	sort_bubble(arr, 0, 10);
+	//sort_bubble(arr, 0, 10);
+	optimized_bubble_sort(arr, 0, 10);
 
 	for(register int i=0; i<10; ++i)
 		printf("%d ", arr[i]);
 
 	return 0;
 }
+#endif
