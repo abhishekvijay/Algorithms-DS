4edc2917abb7b5cb8a7f8b5b3ea96720b13c4b5e
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-05-03 14:20:54 +0530

    adding recur bubble sort

--- Sorting/00_main.cpp
+++ Sorting/00_main.cpp
@@ -1,20 +1,41 @@
 
 #include <stdio.h>
 
+#define MAX			10
+
+#define PRINT(X,A)	do { \
+						printf(X); \
+						for(register int i=0; i<MAX; ++i) \
+							printf("%d ", A[i]); \
+						printf("\n"); \
+					} while(0); \
+
 //Bubble Sort
-extern void sort_bubble(int arr[], int st, int en);
-extern void optimized_bubble_sort(int arr[], int st, int en);
+extern void iter_bubble_sort(int *ptr, int st, int en);
+extern void recur_bubble_sort(int *ptr, int sz);
+extern void opt_bubble_sort(int *ptr, int st, int en);
+
+//Merge Sort
+extern void merge_sort_recur(int *ptr, int st, int en);
+extern void merge_sort_iter(int *ptr, int st, int en);
+
+
 
 
 int main()
 {
-	int arr[10] = {67,100,32,91,61,72,89,45,12,34};
+	int arr[MAX] = {67,100,32,91,61,72,89,45,12,34};
+	int sz = MAX-1;
+	
+	PRINT("Before - ",arr);
 
-//	sort_bubble(arr, 0, 10);
-	optimized_bubble_sort(arr, 0, 10);
+	iter_bubble_sort(arr, 0, sz);
+	recur_bubble_sort(arr, sz);
+	opt_bubble_sort(arr, 0, sz);
 
-	for(register int i=0; i<10; ++i)
-		printf("%d ", arr[i]);
+//	merge_sort_recur(arr, 0, sz);
+//	merge_sort_iter(arr, 0, sz);
 
+	PRINT("After - ", arr);	
 	return 0;
 }
--- Sorting/01_Bubble_Sort.cpp
+++ Sorting/01_Bubble_Sort.cpp
@@ -4,46 +4,58 @@
 #define SWAP(x,y)	do { int temp = *x; *x = *y; *y = temp; } while(0);
 
 
-void optimized_bubble_sort(int arr[], int st, int en)
+void opt_bubble_sort(int *ptr, int st, int en)
 {
 	if (en <= st)
 		return;
 
-	int *ptr = arr;
+	int *ptr1 = ptr;
 	int pass, i;
 	int IsFound = 1;
 
-	for(pass = 0; pass < en-1 && IsFound; ++pass)
+	for(pass = 0; pass < en && IsFound; ++pass)
 	{
 		IsFound = 0;
 
-		for(i=0; i<en-pass-1; ++i) //applied opitmization
+		for(i=0; i<en-pass; ++i) //applied opitmization
 		{
-			if (ptr[i] > ptr[i+1])
+			if (ptr1[i] > ptr1[i+1])
 			{
 				IsFound = 1;
-				SWAP(&ptr[i], &ptr[i+1]);
+				SWAP(&ptr1[i], &ptr1[i+1]);
 			}
 		}
 	}
 }
 
-void sort_bubble(int arr[], int st, int en)
+void recur_bubble_sort(int *ptr, int sz)
+{
+	if (sz == 1)
+		return;
+
+	for(register int i=0; i<sz; ++i)
+		if (ptr[i] > ptr[i+1])
+			SWAP(&ptr[i], &ptr[i+1]);
+
+	recur_bubble_sort(ptr, sz-1);
+}
+
+void iter_bubble_sort(int *ptr, int st, int en)
 {
 	if (en <= st)
 		return;
 
-	int *ptr = &arr[0];
+	int *ptr1 = ptr;
 	int IsFound = 1;
 	int i = 0;
 
 	while(IsFound) {
 		IsFound = 0;
-		for(i = 0; i < en-1; ++i)
+		for(i = 0; i < en; ++i)
 		{
-			if (ptr[i] > ptr[i+1])
+			if (ptr1[i] > ptr1[i+1])
 			{
-				SWAP(&ptr[i], &ptr[i+1]);
+				SWAP(&ptr1[i], &ptr1[i+1]);
 				IsFound = 1;
 			}
 		}
