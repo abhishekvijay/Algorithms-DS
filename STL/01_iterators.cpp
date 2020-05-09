307ff671e30114718b76f514c85294e16e32b042
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-05-09 10:28:18 +0530

    adding bidirectional iterator

--- STL/01_iterators.cpp
+++ STL/01_iterators.cpp
@@ -3,10 +3,22 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <list>
 
 using namespace std;
 
-void gerneral_functions(void)
+/* Iteratory types
+Output			unidirectional		Ofstream, inserter
+Input			unidirectional		Istream
+Forward			unidirectional		forward_list, unordered containers
+Bi-directional	both direction		list, map, multi-map, set, multi-set
+Random Access	both direction		Array, c-style array, vector, string, deque
+
+stack, queue, priority-que - no iterator support
+*/
+
+/*begin(), end(), advance(), next(), prev(), copy(), inserter() */
+void general_functions(void)
 {
 	int iarr[10] = {10,20,30,40,50,60,70,80,90,100};
 	
@@ -73,8 +85,8 @@ void input_iterator(void)
 	 * dereferencing - *ptr, access ptr->m (m = member function)
 	 * incrementable - ++ only, no --
 	 * swappable - 2 iterator elements can be swapped
-	 *
 	 * used - copy(), find()
+	 * arithmetic calculation - not allowed - A+1,A+2
 	 */
 }
 
@@ -87,6 +99,7 @@ void output_iterator(void)
 	 * incremental - only use ++, no --
 	 * swappable - 2 iterators can be swapped
 	 * used - move, find algorithms
+	 * arithmetic calculation - not allowed - A+1,A+2
 	 */
 }
 
@@ -98,13 +111,61 @@ void forward_iterator(void)
 	 * dereferencing - same as input & output iterators
 	 * incremental - ++A, A++ Ok, --A, A-- not OK
 	 * swappable - 2 iterators can be swapped
-	 * used - replace, reverse_copy
+	 * used - replace(), reverse_copy()
+	 * arithmetic calculation - not allowed - A+1,A+2
 	 */
+
+	int arr[10] = {9,8,7,6,5,4,3,2,1,10};
+
+	vector<int> coll;
+
+	register int i;
+	for(i=0; i<10; ++i)
+		coll.push_back(arr[i]);
+
+	vector<int>::iterator pos;
+
+	for(pos = coll.begin(); pos < coll.end(); ++pos)
+		*pos = 100; //updating iterator value
+
+	for(i = 0; i<10; ++i)
+		cout<<" "<<arr[i]<<endl;
+		
+	for(pos = coll.begin(); pos < coll.end(); ++pos)
+		cout<<" "<<(*pos)<<endl;
 }
 
 void bidirectional_iterator(void)
 {
 
+	/* bi-directional, can read & write
+	 * usability - multi-pass algorithms
+	 * operators - allowed(==, !=)
+	 * dereferencing - *A, A->m
+	 * incremental - A++, ++A
+	 * decremental - A--, --A
+	 * swappable - yes
+	 * used - reverse_copy(), random_shuffle()
+	 * can be used in place of forward, input & output iterators
+	 * Arithmetic operation - not allowed
+	 */
+	int arr[10] = {9,8,7,6,5,4,3,2,1,10};
+
+	list<int> coll;
+	list<int>::iterator pos;
+
+	register int i;
+	for(i=0; i<10; ++i)
+		coll.push_back(arr[i]);
+
+	for(pos = coll.end(); pos!=coll.begin(); --pos)
+	{
+		if (pos != coll.end()) {
+			cout<<" "<<(*pos)<<endl;
+		}
+	}
+	cout<<" "<<(*pos)<<endl; //last value from reverse
+	return;
 }
 
 void random_access_iterator(void)
@@ -114,13 +175,13 @@ void random_access_iterator(void)
 
 void check_iterator()
 {
-	general_functions();
+//	general_functions();
 
-	input_iterator();
+//	input_iterator();
 
-	output_iterator();
+//	output_iterator();
 
-	forward_iterator();
+//	forward_iterator();
 
 	bidirectional_iterator();
 
