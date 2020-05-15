cc1959b9670ffcc22b053bbbe60d1bc0dc847088
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-05-15 22:53:03 +0530

    update main

--- DynamicProgramming/00_main_dpg.cpp
+++ DynamicProgramming/00_main_dpg.cpp
@@ -3,15 +3,21 @@
 
 #include <stdio.h>
 
-extern void ugly_numbers();
-extern void fibonacci_num();
-
+extern void ugly_numbers(void);
+extern void fibonacci_num(void);
+extern void catalan_num(void);
+extern void bell_number(void);
+extern void binomial_coeff(void);
+extern void permutation_coeff(void);
 
 int main()
 {
 	ugly_numbers();
-
 	fibonacci_num();
+	catalan_num();
+	bell_number();
+	binomial_coeff();
+	permutation_coeff();
 
 	return 0;
 }
