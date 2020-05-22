71b533130275477c1a8c678ccdc2f08748fa6640
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-05-22 16:48:25 +0530

    check words in string

--- Tree/01_trie.cpp
+++ Tree/01_trie.cpp
@@ -3,10 +3,15 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 #define MAX			26
+
+/*############################################################## basic trie */
 
 typedef struct T_NODE {
 	int isLeaf;
@@ -195,8 +200,100 @@ void basic_trie()
 	return;
 }
 
+/*############################################################## check string */
+
+typedef struct SNODE {
+	char ch;
+	int isEnd;
+	SNODE *child[MAX];
+} SNode;
+
+SNode *getObj()
+{
+	SNode *node = NULL;
+
+	node = (SNode *)malloc(sizeof(SNode));
+	if (node != NULL) {
+		node->ch = '\0';
+		node->isEnd = 0;
+
+		for(int i=0; i<MAX; ++i)
+			node->child[i] = NULL;
+	}
+	return node;
+}
+
+void insert_t(SNode *tri, char *str)
+{
+	if (tri == NULL || str == NULL)
+		return;
+
+	int indx = 0;
+	SNode *hd = tri;
+
+	while(*str) 
+	{
+		indx = *str - 'a';
+
+		if(hd->child[indx] == NULL)
+		{
+			hd->child[indx] = getObj();
+			hd->child[indx]->ch = *str;			
+		}
+
+		hd = hd->child[indx];
+		str += 1;
+	}
+	hd->isEnd = 1;
+	return;
+}
+
+int search_t(SNode *hd, char *str)
+{
+	int indx = 0;
+	SNode *tri = hd;
+
+	while(*str) 
+	{
+		indx = *str - 'a';
+
+		if(tri->child[indx] == NULL)
+			return 0;
+
+		tri = tri->child[indx];
+		str += 1;
+	}
+	return tri->isEnd;
+}
+
+void check_string()
+{
+	char str[] = "this is a bigger string than anything";
+	char subs[10][9] = {"this","there","is","pluto","a","strings","bigger","bigs","string","than"};
+
+	SNode *tri = getObj();
+
+	for(auto i=0; i<10; ++i)
+		insert_t(tri, subs[i]);
+
+	char *ptr, *rest;
+
+	ptr = strtok_s(str, " ", &rest);
+
+	while(ptr != NULL)
+	{
+		cout << " "<<ptr<<" - "<<search_t(tri, ptr) << endl;
+		ptr = strtok_s(NULL, " ", &rest);
+	}
+
+	return;
+}
+
 void trie()
 {
+	/*insert, search, delete in a trie*/
 	basic_trie();
 
+	/*Check if given words are present in a string*/
+	check_string();
 }
