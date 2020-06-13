6f554df488f6f930e6bbae4fcc64d06f6424d9a3
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-06-13 22:42:48 +0530

    updating for shared pointer call

--- STL/00_main_stl.cpp
+++ STL/00_main_stl.cpp
@@ -13,6 +13,8 @@ extern void check_dequeue(void);
 extern void check_arrays(void);
 extern void check_forward_list(void);
 extern void check_pair(void);
+extern void check_tuple(void);
+extern void check_smart_pointers(void);
 
 //container adaptors
 //associative containers
@@ -30,9 +32,13 @@ int main()
 	check_list();
 	check_dequeue();
 	check_arrays();
-	check_forward_list();
+	check_forward_list();	
 */
+
+	/* stl utilities */
 	check_pair();
+	check_tuple();
+	check_smart_pointers();
 
 	/* container adaptors */
 	/* associative containers */
