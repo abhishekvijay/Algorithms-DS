980e1160e699142b529cdfcfa93189a625ef4c45
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-06-20 12:53:41 +0530

    add map implementation

---
+++ STL/11_multimap.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <string>
+#include <map>
+
+
+template <typename T>
+void printMapElements(const T& coll) {
+	for(const auto& p : coll)
+		std::cout<<"["<<p.first<<","<<p.second<<"]"<<std::endl;
+	std::cout<<std::endl;
+	return;
+}
+
+void check_multimap(void)
+{
+	std::multimap<int, std::string> mm;
+
+	//insert
+	std::pair<int, std::string> p1 (100000, "MNO");
+	std::pair<int, std::string> p2 (10000, "JKL");
+	std::pair<int, std::string> p3 (1000, "GHI");
+	std::pair<int, std::string> p4 (100, "DEF");
+	std::pair<int, std::string> p5 (1, "ABC");
+	std::pair<int, std::string> p6 (100, "DEF");
+	std::pair<int, std::string> p7 (1000, "GHI");
+	std::pair<int, std::string> p8 (10000, "JKL");
+	std::pair<int, std::string> p9 (100000, "MNO");
+
+	mm.insert(p1); mm.insert(p2);
+	mm.insert(p3); mm.insert(p4);
+	mm.insert(p5); mm.insert(p6);
+	mm.insert(p7); mm.insert(p8);
+	mm.insert(p9);	
+
+	//print
+	for (const auto& p : mm)
+		std::cout<<"["<<p.first<<","<<p.second<<"]"<<std::endl;
+
+	std::cout<<std::endl;
+
+	//template way
+	printMapElements(mm);
+
+	//size
+	std::cout<<"size - "<<mm.size()<<std::endl;
+
+	std::cout<<std::endl;
+
+	return;
+}
