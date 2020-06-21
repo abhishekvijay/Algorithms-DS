59ec446ebb8b8d59c8d4e72fcfde2ee3fd77fbc9
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-06-21 11:26:44 +0530

    iterator with multiset

--- STL/10_multiset.cpp
+++ STL/10_multiset.cpp
@@ -33,6 +33,18 @@ void check_multiset(void)
 	//template way
 	printSetElements(ms);
 
+	//const-iterator - read only mode
+	std::multiset<std::string>::const_iterator citr;
+	for(citr = ms.begin(); citr != ms.end(); ++citr)
+		std::cout<<" "<<*citr;
+	std::cout<<std::endl;
+
+	//iterator - read/write mode
+	std::multiset<std::string>::iterator itr;
+	for(itr = ms.begin(); itr != ms.end(); ++itr)
+		std::cout<<" "<<*itr;
+	std::cout<<std::endl;
+
 	//size
 	std::cout<<"size - "<<ms.size()<<std::endl;	
 
