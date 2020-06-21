7d4df626fd5c64b3c1c69beb382ad900beb94052
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-06-21 11:21:41 +0530

    iterators with forward list

--- STL/09_forward_list.cpp
+++ STL/09_forward_list.cpp
@@ -24,21 +24,28 @@ void check_forward_list()
 	//print elements
 	for(const auto& l : fl)
 		std::cout<<" "<<l;
-
 	std::cout<<std::endl;
 
 	//template way
 	printFLElements(fl);
 
+	//const-iterator - read only mode
+	std::forward_list<char>::const_iterator citr;
+	for(citr = fl.begin(); citr != fl.end(); ++citr)
+		std::cout<<" "<<*citr;
+	std::cout<<std::endl;
+
+	//iterator - read/write mode
+	std::forward_list<char>::iterator itr;
+	for(itr = fl.begin(); itr != fl.end(); ++itr)
+		std::cout<<" "<<*itr;
 	std::cout<<std::endl;
 
 	//collection way
-	while (!fl.empty())
-	{
+	while (!fl.empty()) {
 		std::cout<<" "<<fl.front();
 		fl.pop_front();
 	}
-
 	std::cout<<std::endl;
 
 	return;
