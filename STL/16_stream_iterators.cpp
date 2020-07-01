332157babf7e558301b123c7e6f0ee18be71695c
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-07-01 06:09:42 +0530

    adding stream iterator

--- STL/16_stream_iterators.cpp
+++ STL/16_stream_iterators.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <istream>
+#include <ostream>
+#include <algorithm>
 
 void check_stream_iterator_adapter(void)
 {
+	std::vector<std::string> coll;
+
+	//reading input from std::cin
+	std::copy(std::istream_iterator<std::string>(std::cin),
+				std::istream_iterator<std::string>(),
+				std::back_inserter(coll));
+
+	//sorting elements
+	std::sort(coll.begin(), coll.end());
+
+	//copy to std::cout
+	std::unique_copy(coll.begin(), coll.end(), 
+				std::ostream_iterator<std::string>(std::cout, "\n"));
+	
+	std::cout<<std::endl;
 	return;
 }
