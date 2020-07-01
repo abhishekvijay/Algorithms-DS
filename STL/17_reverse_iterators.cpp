02c069864c5b5252984294b104c0b5fd4292d51a
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-07-01 06:15:38 +0530

    adding reverse iterator

--- STL/17_reverse_iterators.cpp
+++ STL/17_reverse_iterators.cpp
@@ -5,5 +5,14 @@
 
 void check_reverse_iterator_adapter(void)
 {
+	std::vector<int> coll;
+
+	for(int i = 1; i <= 10; ++i)
+		coll.push_back(i);
+
+	std::copy(coll.crbegin(), coll.crend(), std::ostream_iterator<int>(std::cout, "\n"));
+
+	std::cout<<std::endl;
+
 	return;
 }
