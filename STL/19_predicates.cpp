4cca5d228215c30090e5ab35cf8a6466ad65a3e0
Abhsihek Vijay <abhishekvijay@gmail.com>
2020-07-05 12:34:38 +0530

    add binary  predicates

--- STL/19_predicates.cpp
+++ STL/19_predicates.cpp
@@ -1,8 +1,10 @@
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <unordered_map>
 
 /*
 A predicate is a special kind of auxiliary function. Predicates return a Boolean value and are often
@@ -19,6 +21,13 @@ is used as a search criterion to find the first prime number
 Binary predicates typically compare a specific property of two arguments. For example, to sort
 elements according to your own criterion, you could provide it as a simple predicate function.
 */
+template <typename T>
+void printPrElements(const T& coll)
+{
+	for (auto const& p : coll)
+		std::cout<<"["<<std::get<0>(p)<<","<<std::get<1>(p)<<"]"<<std::endl;
+	return;
+}
 
 bool isPrime(int num)
 {
@@ -37,6 +46,7 @@ bool isPrime(int num)
 		if ((num % i == 0) || (num % (i+2) == 0))			
 			return false;		
 	}
+	return true;
 }
 
 void unary_predicates()
@@ -58,8 +68,40 @@ void unary_predicates()
 	return;
 }
 
+typedef std::pair<int, std::string> paer;
+
+bool sortCriteria(const paer& T1, const paer& T2)
+{	
+	int num1 = std::get<0>(T1);
+	int num2 = std::get<0>(T2);
+
+	if (num1 < num2) 
+		return true;
+	else
+		return false;
+}
+
 void binary_predicates()
 {
+	std::unordered_map<int, std::string> coll;
+
+	paer p1(100000, "MNO");
+	paer p2(10000, "JKL");
+	paer p3(10, "ABC");
+	paer p4(100, "DEF");
+	paer p5(1000, "GHI");
+
+	coll.insert(p1); coll.insert(p2);
+	coll.insert(p3); coll.insert(p4);
+	coll.insert(p5);
+
+	//correct sorting criteria
+	//std::sort(coll.begin(), coll.end(), sortCriteria);
+
+	printPrElements(coll);
+
+	std::cout<<std::endl;
+
 	return;
 }
 
